da40654953b147ae2124e3c4dd05a4b604c561de
Arvin <arvinjohnyadao@hotmail.com>
2023-01-17 20:46:15 +0800

    verbiage

--- cypress/e2e/factoriall-calculate-spec.cy.js
+++ cypress/e2e/factoriall-calculate-spec.cy.js
@@ -3,14 +3,14 @@ import HomePage from '../pages/home_page'
 import positiveData from '../fixtures/positive_test_data.json'
 import negativeData from '../fixtures/negative_test_data.json'
 
-describe('End to End test for calculating factorial of a number', () => {
+describe('End to End UI test for calculating factorial of a number', () => {
   beforeEach(() => {
     cy.visit("/")
   })
 
   // using parametrized integer test data
   positiveData.forEach(data => {
-    it(`returns correct result when using valid data. Input: ${data.input}`, () => {
+    it(`displays correct result when using valid data. Input: ${data.input}`, () => {
       HomePage.enterAnInteger(data.input)
       HomePage.calculateFactorial()
       HomePage.getFactorialResult().should('have.text', `The factorial of ${data.input} is: ${data.output}`)
@@ -19,7 +19,7 @@ describe('End to End test for calculating factorial of a number', () => {
 
   // using parametrized non integer test data
   negativeData.forEach(data => {
-    it(`returns error message when using invalid data. Input: ${data.input}`, () => {
+    it(`displays error message when using invalid data. Input: ${data.input}`, () => {
       HomePage.enterAnInteger(data.input)
       HomePage.calculateFactorial()
       HomePage.getFactorialResult().should('have.text', 'Please enter an integer')
